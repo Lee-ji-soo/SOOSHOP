ea5a7f6f635450b20a1faae133594df50ce75bcf
Lee-ji-soo <nbsp3@naver.com>
2020-06-22 00:25:13 +0900

    cart 가격 더하기 수정  + KRW으로 변환(문제있음 각 상품의 가격이 수정이 KRW으로 안됨)

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SOOSHOP
 shoppingmall_website
+
+## cart
+1. 정보 localStorage에 저장
+
+2. 각 정보 + 가격 출력 + 코리안 won으로 변경
+
+3. minus,plus 클릭 시 가격 경
+4. total 가격 변경 + 코리안 won 으로 변경
+5. minus,plus sibling 가격 출력 + 코리안 won으로 변경
+   
--- js/cart.js
+++ js/cart.js
@@ -24,13 +24,12 @@ dataArr.push(data1, data2, data3, data4, data5);
 for (i = 0; i < dataArr.length; i++) {
     $('.table').append(
         `<div class="cart-boxs" data-id="${dataArr[i].id}">
-        <div scope="row" class="cartBox" id="dataId"data-id="${dataArr[i].id}"></div>
-        <div class="cart-boxs__img">
-          <img src="${dataArr[i].img}">
-        </div>
-        
-        <div class="cart-boxs__detail">
-          <div class="price">${dataArr[i].price}</div>
+          <div scope="row" class="cartBox"              id="dataId" data-id="${dataArr[i].id}"></div>
+          <div class="cart-boxs__img">
+            <img src="${dataArr[i].img}">
+          </div>
+          <div class="cart-boxs__detail">
+          <div data-id="${dataArr[i].id}" class="price">${dataArr[i].price}</div>
           <div class='name'>${dataArr[i].name}</div>
           <div><i class="fas fa-minus minus"></i><span class="amount">${dataArr[i].quantity}</span><i class="fas fa-plus plus"></i></div>
           <div>사이즈 ${dataArr[i].size}</div>
@@ -39,34 +38,48 @@ for (i = 0; i < dataArr.length; i++) {
           <i class="fas fa-trash-alt  delete"></i>
         </div>
       </div>`)
+
     localStorage.setItem(`product${dataArr[i].id}`, JSON.stringify(dataArr[i]))
 }
+//한국 돈으로 프린트
+const cartBox = $('.cart-boxs');
+var eachPrice = $('.price');
 
-//한국돈으로 표시하기
-let cartBox = $('.cart-boxs')
-let eachPrice = $('.price')
-for (i = 0; i < cartBox.length; i++) {
-    let eachPriceHTML = eachPrice[i].innerHTML
-    let eachPriceInKor = Number(eachPriceHTML).toLocaleString()
-    // let eachPriceInKor = eachPriceHTML[i].toLocaleString();
-    eachPrice[i].innerHTML = `₩${eachPriceInKor}`
+for (i = 0; i < eachPrice.length; i++) {
+
+    let willChange = eachPrice[i].innerHTML
+    let printKorSumEach = new Intl.NumberFormat('ko-KR', {
+        style: 'currency',
+        currency: 'KRW'
+    }).format(willChange);
 }
 
 //합계구하기
 function sum() {
-    price = $('.price');
     let sum = 0;
-    for (i = 0; i < price.length; i++) {
-        sum += Number(price[i].innerHTML);
+    for (i = 0; i < eachPrice.length; i++) {
+        sum += Number(eachPrice[i].innerHTML);
     }
-    var sumKor = sum.toLocaleString();
-    $('#sum').html(`₩${sumKor}`);
-}
+    var semisum = $('#sum').html(sum)
+    // krw으로 변경
+    var korSum = semisum.html()
+    //가격 내용 krw으로 변경
 
+    let printKorSum = new Intl.NumberFormat('ko-KR', {
+        style: 'currency',
+        currency: 'KRW'
+    }).format(korSum);
+
+    let newPrint = semisum.html(printKorSum);
+
+    ("#sum").innerHTML = newPrint;
+}
 
 //마이너스키
 function minus(e) {
-    var jprice = dataArr[`${$(e.target).parent().siblings('#dataId').html()}` - 1].price;
+    var etargetID = e.target.parentNode.parentNode.parentNode.dataset.id - 1;
+
+    var jprice = dataArr[etargetID].price;
     var amount = $(e.target).siblings('span').html();
     if (amount > 0) {
         //1씩줄어듬
@@ -80,28 +93,33 @@ function minus(e) {
         amount = 0
     }
     sum();
+
 }
 
 //플러스키
+
 function plus(e) {
-    var jprice = dataArr[`${$(e.target).parent().siblings('#dataId').html()
-        }` - 1].price;
+    var etargetID = e.target.parentNode.parentNode.parentNode.dataset.id - 1;
+
+    var jprice = dataArr[etargetID].price;
+
     var amount = $(e.target).siblings('span').html();
     if (amount < 10) {
         //1씩줄어듬
         $(e.target).siblings('span').html(Number(amount) + 1)
         amount = $(e.target).siblings('span').html();
         $(e.target).parent().siblings('.price').html(jprice * amount);
-
     } else {
         amount = 10
     }
     sum();
 }
+
 $('.minus').on('click', minus);
 $('.plus').on('click', plus);
 sum();
 
+//삭제하기
 function deleteEle(e) {
     var clicked = $(e.target).parent().parent();
     var clickedId = clicked.data('id');
@@ -112,5 +130,3 @@ function deleteEle(e) {
     }
 }
 $('.delete').on('click', deleteEle);
-
-
