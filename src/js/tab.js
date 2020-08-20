36eef8fe18da84005fa607c7acebe93c87ede0e9
Lee-ji-soo <nbsp3@naver.com>
2020-08-20 13:04:53 +0900

    rm -r node_modules

--- src/js/tab.js
+++ src/js/tab.js
@@ -1,19 +1,23 @@
-'use strict';
-
 var 버튼들 = document.querySelectorAll('.tab-button');
 var 탭내용들 = document.querySelectorAll('.tab-content');
 
-document.querySelector('.list').addEventListener('click', function (e) {
-  //  탭보여주는거( 내가실제누른요소에숨겨진data-id값 );
-  탭보여주는거(e.target.dataset.id);
-});
 
-function 탭보여주는거(i) {
+document.querySelector('.list').addEventListener('click', function(e){
+//  탭보여주는거( 내가실제누른요소에숨겨진data-id값 );
+  탭보여주는거( e.target.dataset.id );
+})
+
+
+function 탭보여주는거(i){
   document.querySelector('.orange').classList.remove('orange');
-  버튼들[i].classList.add('orange');
+  버튼들[i].classList.add('orange');  
   document.querySelector('.show').classList.remove('show');
   탭내용들[i].classList.add('show');
 }
+
+
+
+
 
 //for (let i = 0; i < 3; i++){
 //  버튼들[i].addEventListener('click', function(){
@@ -28,6 +32,9 @@ function 탭보여주는거(i) {
 //}
 
 
+
+
+
 //버튼1을 누르면...
 //
 //  버튼0,1,2에있던 orange 클래스명 다 제거 
@@ -35,3 +42,12 @@ function 탭보여주는거(i) {
 //  
 //  내용0,1,2에있던 show 클래스명 다 제거
 //  내용1에 show라는 class명추가
+
+
+
+
+
+
+
+
+
